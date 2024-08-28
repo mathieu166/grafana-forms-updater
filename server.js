aac3b5a40745459b85336ae7f810cebb725b026e
Mathieu Riendeau <riendeau_mathieu@hotmail.com>
2024-08-27 20:08:02 -0400

    get validator

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-var cors = require('cors');
 const { Client } = require('pg'); 
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3003;
 
@@ -17,10 +15,7 @@ const client = new Client({
 
 // Connect to PostgreSQL
 client.connect();
-
-// Middleware to parse JSON request bodies
-app.use(bodyParser.json()); // Ensure this is placed before your routes
-app.use(cors());
+app.use(express.json()); 
 
 // Default GET route
 app.get('/', (req, res) => {
@@ -29,8 +24,6 @@ app.get('/', (req, res) => {
 
 // Endpoint to update or insert validator specs
 app.post('/validator/update', async (req, res) => {
-  console.log('Request Body:', req.body); // Log the request body to debug
-
   const { address, ip_host, port } = req.body;
 
   try {
@@ -48,6 +41,29 @@ app.post('/validator/update', async (req, res) => {
     res.status(500).send('An error occurred while updating/inserting validator spec.');
   }
 });
+
+app.get('/validator/:address', async (req, res) => {
+  const { address } = req.params;
+
+  try {
+    const query = `
+      SELECT * FROM validator_specs
+      WHERE address = $1;
+    `;
+    
+    const result = await client.query(query, [address.toLowerCase()]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).send('Validator not found.');
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error('Error retrieving validator spec:', error);
+    res.status(500).send('An error occurred while getting validator specs.');
+  }
+});
+
 
 // Start the server
 app.listen(PORT, () => {
